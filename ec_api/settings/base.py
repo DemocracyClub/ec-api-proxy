2d0105cf6127d9927c795adac128eb694a8c470a
Sym Roe <sym.roe@talusdesign.co.uk>
2021-01-28 14:02:19 +0000

    Don't store default SECRET_KEY
    Take from the environment or fail

--- ec_api/settings/base.py
+++ ec_api/settings/base.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "54ec%)_i)i83lr0etzt=q#1%b!%66+@)c%m!6tx1!63^+(tccd"
+SECRET_KEY = os.environ.get("SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
