d1f940fd78a1418d2c68d59d306a0bedb8b4ad59
Sym Roe <sym.roe@talusdesign.co.uk>
2021-02-03 11:37:06 +0000

    Add base lambda settings
    This file is used for all envs that are deployed on AWS Lambda

---
+++ ec_api/settings/base_lambda.py
@@ -0,0 +1,36 @@
+from .base import *  # noqa
+import os
+
+ALLOWED_HOSTS = ["*"]
+DEBUG = os.environ.get("DEBUG", False)
+
+if os.environ.get("APP_IS_BEHIND_CLOUDFRONT", False) in [
+    True,
+    "true",
+    "True",
+    "TRUE",
+]:
+    USE_X_FORWARDED_HOST = True
+else:
+    USE_X_FORWARDED_HOST = False
+    FORCE_SCRIPT_NAME = "/Prod"
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.dummy"}}
+
+INSTALLED_APPS.remove("django.contrib.admin")
+INSTALLED_APPS.remove("django.contrib.sessions")
+INSTALLED_APPS.remove("django.contrib.messages")
+
+AWS_S3_SECURE_URLS = False
+AWS_S3_USE_SSL = True
+AWS_S3_REGION_NAME = "eu-west-2"
+AWS_QUERYSTRING_AUTH = False
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get(
+    "AWS_STORAGE_BUCKET_NAME", "ec-api-static-assets-development-32sdf4sg4"
+)
+AWS_S3_CUSTOM_DOMAIN = (
+    f"{AWS_STORAGE_BUCKET_NAME}.s3-website.{AWS_S3_REGION_NAME}.amazonaws.com"
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
