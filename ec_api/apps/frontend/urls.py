0845b214311b87bbfafae596db15f919622c895c
Sym Roe <sym.roe@talusdesign.co.uk>
2021-03-10 13:52:11 +0000

    Add Widget view

--- ec_api/apps/frontend/urls.py
+++ ec_api/apps/frontend/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from ec_api.apps.frontend import views
 
 urlpatterns = [
-    path("", views.HomePageView.as_view()),
+    path("", views.HomePageView.as_view(), name="home_view"),
+    path("widget/", views.WidgetView.as_view(), name="widget_view"),
+    path("widget/widget.js", views.WidgetJSView.as_view(), name="widget_js"),
 ]
