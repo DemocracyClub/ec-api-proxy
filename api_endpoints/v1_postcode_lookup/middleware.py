0617dc8153c4b7e2b676375a14b3a2dba9a9df0f
Sym Roe <sym.roe@talusdesign.co.uk>
2023-04-18 16:39:45 +0100

    Pass API Key on to DevsDC backend

--- api_endpoints/v1_postcode_lookup/middleware.py
+++ api_endpoints/v1_postcode_lookup/middleware.py
@@ -41,7 +41,49 @@ class SimpleCORS:
         await self.app(scope, receive, send_with_cors_headers)
 
 
+class APIGatewayAuthenticatorContextMiddleware:
+    """
+    Some app functions are behind an AWS API Gateway Authorizer.
+
+    The Authorizer will control access to the Lambdas functions "behind" it,
+    and will also insert context data about the authenticated user.
+
+    We want to use this data when logging access, so this middleware parses the
+    context in to a User model that can be used by functions.
+
+    There are two other cases to consider:
+
+    1. When we're on AWS Lambda but running an unauthenticated function.
+       We don't know who the user is in this case, so we can just create a fake user
+    2. When we're not on AWS Lambda, like when running tests or local dev.
+
+    """
+
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        aws_event = scope.get("aws.event", {})
+        authorizer_data = aws_event.get("requestContext", {}).get(
+            "authorizer", {}
+        )
+
+        if authorizer_data:
+            # We've been passed through an authorizer so we can create a User model
+            api_key = authorizer_data["auth_token"]
+        elif aws_event:
+            # We're on AWS Lambda, but this isn't a function with an authorizer
+            api_key = "unauthenticated_user"
+        else:
+            # We're not on AWS Lambda
+            api_key = "direct_access"
+
+        scope["api_user"] = api_key
+        await self.app(scope, receive, send)
+
+
 MIDDLEWARE = [
     Middleware(ForwardedForMiddleware),
     Middleware(SimpleCORS),
+    Middleware(APIGatewayAuthenticatorContextMiddleware),
 ]
--- api_endpoints/v1_postcode_lookup/upstream_api_client.py
+++ api_endpoints/v1_postcode_lookup/upstream_api_client.py
@@ -16,21 +16,25 @@ DC_API_URL = os.environ.get(
 
 
 class DCApiClient:
-    def get_response_from_upstream(self, path, params=None):
+    def get_response_from_upstream(self, path, api_key, params=None):
         if not params:
             params = {}
-        params["auth_token"] = DC_API_TOKEN
+        params["auth_token"] = api_key
         url = f"{DC_API_URL}/{path}"
         req = session.get(url, params=params)
         req.raise_for_status()
         return req.json()
 
-    def _get_raw_postcode_response(self, postcode):
-        json = self.get_response_from_upstream(f"/api/v1/postcode/{postcode}")
+    def _get_raw_postcode_response(self, postcode, api_key):
+        json = self.get_response_from_upstream(
+            f"/api/v1/postcode/{postcode}", api_key
+        )
         return json
 
-    def _get_raw_uprn_response(self, uprn):
-        json = self.get_response_from_upstream(f"/api/v1/address/{uprn}")
+    def _get_raw_uprn_response(self, uprn, api_key):
+        json = self.get_response_from_upstream(
+            f"/api/v1/address/{uprn}", api_key
+        )
         return json
 
     def clean_candidates(self, candidates):
@@ -70,6 +74,9 @@ class DCApiClient:
         raw_response = self.clean_addresses(request, raw_response)
         return raw_response
 
+    def get_api_key(self, request: Request):
+        return request.scope.get("api_user", DC_API_TOKEN)
+
     def clean_uprn_response(self, request, raw_response):
         # At the moment, cleaning a UPRN response is the same as a postcode
         # response
@@ -77,11 +84,15 @@ class DCApiClient:
         return raw_response
 
     def get_postcode_response(self, request, postcode):
-        raw_response = self._get_raw_postcode_response(postcode)
+        raw_response = self._get_raw_postcode_response(
+            postcode, api_key=self.get_api_key(request)
+        )
         return self.clean_postcode_response(request, raw_response)
 
     def get_uprn_response(self, request, uprn):
-        raw_response = self._get_raw_uprn_response(uprn)
+        raw_response = self._get_raw_uprn_response(
+            uprn, api_key=self.get_api_key(request)
+        )
         return self.clean_uprn_response(request, raw_response)
 
 
