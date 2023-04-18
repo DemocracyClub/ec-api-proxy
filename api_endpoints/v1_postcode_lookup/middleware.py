fcdb65896c2695e0d0de087e99a3659a30e0d6d6
Sym Roe <sym.roe@talusdesign.co.uk>
2023-04-18 14:11:11 +0100

    CORS from app

--- api_endpoints/v1_postcode_lookup/middleware.py
+++ api_endpoints/v1_postcode_lookup/middleware.py
@@ -1,4 +1,4 @@
-from starlette.datastructures import Headers
+from starlette.datastructures import Headers, MutableHeaders
 from starlette.middleware import Middleware
 
 
@@ -23,6 +23,25 @@ class ForwardedForMiddleware:
         await self.app(scope, receive, send)
 
 
+class SimpleCORS:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        if scope["type"] != "http":
+            return await self.app(scope, receive, send)
+
+        async def send_with_cors_headers(message):
+            if message["type"] == "http.response.start":
+                headers = MutableHeaders(scope=message)
+                headers.append("Access-Control-Allow-Origin", "*")
+
+            await send(message)
+
+        await self.app(scope, receive, send_with_cors_headers)
+
+
 MIDDLEWARE = [
     Middleware(ForwardedForMiddleware),
+    Middleware(SimpleCORS),
 ]
