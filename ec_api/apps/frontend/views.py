a59fd20d4c0de05e0e78bdffd5d38c972144c6c3
Sym Roe <sym.roe@talusdesign.co.uk>
2023-11-21 21:35:59 +0000

    Use method decorator on widget

--- ec_api/apps/frontend/views.py
+++ ec_api/apps/frontend/views.py
@@ -1,5 +1,6 @@
 import requests
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.cache import cache_control
 from django.views.generic import TemplateView
@@ -16,7 +17,7 @@ class WidgetView(TemplateView):
 
 
 class WidgetJSView(View):
-    @cache_control(max_age=600)
+    @method_decorator(cache_control(max_age=600))
     def get(self, request, *args, **kwargs):
         req = requests.get(WIDGET_S3_URL)
         req.raise_for_status()
